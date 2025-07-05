1335e1a891580bb94eb4fee77a0a8def7225f2fe
whan05 <wfonsecap-m@ucenfotec.ac.cr>
2025-07-04 22:43:34 -0600

    integrated build folder into public

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express");
 const { dbConection } = require("./database/config");
 const cors = require("cors");
@@ -21,6 +22,10 @@ app.use(express.json());
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+app.use( '*', ( req, res) => (
+  res.sendFile( path.join( __dirname, 'public/index.html'))
+))
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
